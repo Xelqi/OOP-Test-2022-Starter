bb586269f0414e9d1dc6d7677f314ed8e3203d33
Xelqi <milek1093@gmail.com>
2022-04-27 16:16:25 +0100

    adding the constructor and filling it

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,50 @@
+package ie.tudublin;
+
+public class Nematode {
+
+    private String name;
+    private float length;
+    private boolean limbs;
+    private char gender;
+    private boolean eyes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public boolean isLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public boolean isEyes() {
+        return eyes;
+    }
+
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    }
+}
